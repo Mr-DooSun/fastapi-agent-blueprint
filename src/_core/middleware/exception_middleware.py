16af89d1e3e64557b4fdeb5b2f99a833fb5f92b9
Mr-DooSun <mrdoosun@gmail.com>
2025-12-07 01:15:41 +0900

    [#55] fix: enhance error handling in ExceptionMiddleware to conditionally include error trace based on environment settings

--- src/_core/middleware/exception_middleware.py
+++ src/_core/middleware/exception_middleware.py
@@ -6,6 +6,7 @@ from fastapi.responses import JSONResponse
 from starlette.middleware.base import BaseHTTPMiddleware
 
 from src._core.application.dtos.base_response import ErrorResponse
+from src._core.config import settings
 from src._core.exceptions.base_exception import BaseCustomException
 
 
@@ -28,11 +29,13 @@ class ExceptionMiddleware(BaseHTTPMiddleware):
             error_trace = traceback.format_exc()
             print(error_trace)
             # ===================================
+            error_details = {"trace": error_trace} if settings.is_dev else None
+
             content = jsonable_encoder(
                 ErrorResponse(
                     message="Internal server error",
                     error_code="INTERNAL_SERVER_ERROR",
-                    error_details={"trace": error_trace},
+                    error_details=error_details,
                 )
             )
             return JSONResponse(
